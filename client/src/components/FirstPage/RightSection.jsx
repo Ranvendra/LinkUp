1df87e7d1cbeeba87692499225da6e3b7f168e28
RanvendraNewton <ranvendra.singh2024@nst.rishihood.edu.in>
2025-11-08 22:25:44 +0530

    Icon Updated

--- client/src/components/FirstPage/RightSection.jsx
+++ client/src/components/FirstPage/RightSection.jsx
@@ -1,6 +1,6 @@
 // import heroimage from "../../assets/hero.jpeg";
 // import girl from "../../assets/girl.jpg";
-import girl2 from "../../assets/girl2.png";
+import girl from "../../assets/model.jpg";
 
 
 
@@ -8,7 +8,7 @@ import girl2 from "../../assets/girl2.png";
 function RightSection() {
   return (
       <img
-        src={girl2}
+        src={girl}
         alt="Scenic mountain lake"
         className="w-full h-screen object-cover"
       />

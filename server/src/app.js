63b5031c090a60dd3e37f730e126bda6346523a2
RanvendraNewton <ranvendra.singh2024@nst.rishihood.edu.in>
2025-12-03 12:56:53 +0530

    hosting issue

--- server/src/app.js
+++ server/src/app.js
@@ -38,6 +38,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.set("trust proxy", 1); // Trust first proxy (Render/Vercel)
 
 // Importing RouTers;
 const authRouter = require("./routes/auth");

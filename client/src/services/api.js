5cdf148fb2e7d2ffb55d0980ee8e192a462cb387
ranvendra <ranvendrapratapsinghofficial@gmail.com>
2026-03-05 18:58:37 +0530

    cookie sent on every request after login

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -15,6 +15,21 @@ const api = axios.create({
   withCredentials: true, // This allows cookies to be sent with requests and is the Axios equivalent of credentials: 'include'
 });
 
+// Add a request interceptor to include the token in headers
+// This is critical for production where strict browser privacy settings might block 3rd-party cookies
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 // Auth API calls
 export const authAPI = {
   signup: (userData) => {
